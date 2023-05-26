fdd9cdad253427b1aca0db378de7e523de509355
NadVNik <128215381+NadVNik@users.noreply.github.com>
2023-05-26 07:08:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def get_pass_list(data, save, sex):
     if sex == 'мужчины':
-        sex = 'male'
+        sex ='male'
     else:
-        sex = 'female'
+        sex ='female'
     out_list = []
     for line in data:
         if line.split(',')[1] == save and line.split(',')[5] == sex:
@@ -12,7 +12,7 @@ def get_pass_list(data, save, sex):
     return out_list
 
 with open('data.csv') as file:
-    data = file.readlines()
+    data =file.readlines()
 
 def MakarNV_code():
     sex = st.selectbox('Выберите пол пассажира:', ['мужчины', 'женщины'])
